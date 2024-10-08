d11d9e63089bb493a9b5f6c14dbb9298626c0641
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2024-10-07 20:53:56 -0400

    api

---
+++ backend/gym-api/gym-api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gym_api.Controllers
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController() { }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById([FromQuery] string RequestSource, int id)
+        {
+            return Ok("User info");
+        }
+
+
+        [Authorize(Roles = "EnterpriseUser")]
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            return Ok("Creating user");
+        }
+
+
+    }
+
+}
